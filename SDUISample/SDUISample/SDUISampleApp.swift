2438b6b0a1a1d8a49be3f9dbc550afaf20423b11
Hilst <hilstfelipe@gmail.com>
2022-11-04 23:03:28 -0300

    show base detail view

--- SDUISample/SDUISample/SDUISampleApp.swift
+++ SDUISample/SDUISample/SDUISampleApp.swift
@@ -6,7 +6,7 @@ struct SDUISampleApp: App {
 
     static private let provider = Provider()
     static private let linker = SampleLinker()
-    static let viewModel = SDUIViewModel(initalRoute: .init(rawValue: "lista"),
+    static let viewModel = SDUIViewModel(initalRoute: .init(rawValue: "um-detail"),
                                          provider: provider,
                                          linker: linker)
 
